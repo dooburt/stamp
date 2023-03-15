3d9f996d7866af9e992f17526ecbad9e301ff7da
dooburt <dooburt@gmail.com>
2023-03-15 09:07:19 +0000

    encrypts, but does not decrypt

--- src/main/decryptor.ts
+++ src/main/decryptor.ts
@@ -1,27 +1,47 @@
 import { pipeline } from 'stream/promises';
 import { createReadStream, createWriteStream } from 'fs';
-import { createCipheriv, randomBytes, createHash } from 'crypto';
+import { createDecipheriv, createHash } from 'crypto';
 
 async function decryptionPipe(
   pathToBoo: string,
   key: string,
-  iv: string,
-  originalLocation: string
+  encodedIv: string,
+  zipLocation: string
 ) {
   // we must store the IV, it is required for decryption
   const hash = createHash('sha256')
     .update(String(key))
     .digest('base64')
     .substr(0, 32);
-  const iv = randomBytes(16).toString('hex').slice(0, 16);
+
+  const iv = Buffer.from(encodedIv, 'hex');
+
+  console.log('pathToBoo', pathToBoo);
+  console.log('key', key);
+  console.log('encodedIv', encodedIv);
+  console.log('iv', iv);
+  // console.log('decoded iv', Buffer.from(iv).toString('hex'));
+  console.log('hash', hash);
+  console.log('zipLocation', zipLocation);
+
   const data = await pipeline(
     createReadStream(pathToBoo),
-    createCipheriv('aes-256-cbc', hash, iv),
-    createWriteStream(pathToBoo)
+    createDecipheriv('aes-256-ctr', hash, iv).setAutoPadding(true),
+    createWriteStream(zipLocation)
   );
-  console.log('Encryption pipe', data, iv, hash);
 
-  return { data, iv, hash };
+  // const hash = createHash('sha256')
+  //   .update(String(key))
+  //   .digest('base64')
+  //   .substr(0, 32);
+  // const data = await pipeline(
+  //   createReadStream(pathToBoo),
+  //   createCipheriv('aes-256-cbc', hash, iv),
+  //   createWriteStream(pathToBoo)
+  // );
+  console.log('Decryption pipe', data, iv);
+
+  return { data };
 }
 
 export default decryptionPipe;
